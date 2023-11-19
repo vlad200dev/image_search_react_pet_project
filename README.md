29bfb0c27bc0196a63a889d210b390b1e53c8267
kvlad <kutvladislaw@gmail.com>
2023-11-19 14:37:42 +0600

    add components, react-query,unsplash api integration, css adaptation

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# Small pet project 
+# Small pet project to fetch photos based on search query
+
+## Use in project:
+1. React
+2. Unspash API
+3. React-query
+4. Vite
 
 #### Setup
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
+import Gallery from './components/Gallery';
+import SearchForm from './components/SearchForm';
+import ThemeToggle from './components/ThemeToggle';
+
 const App = () => {
-    return <h1>Unsplash Images Starter</h1>;
+    return (
+        <main>
+            <ThemeToggle/>
+            <SearchForm/>
+            <Gallery/>
+        </main>
+    );
 };
 export default App;
